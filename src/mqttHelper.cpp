80a8268c614af124fd87d333aaf53683521c29b7
LABMDKPhan <MinhDangKhoa.Phan@ttigroup.com.vn>
2024-12-17 13:22:11 +0700

    Still need to check ip !=0.0.0.0

--- src/mqttHelper.cpp
+++ src/mqttHelper.cpp
@@ -1,7 +1,7 @@
 #include <PubSubClient.h>
 #include "main.h"
 
-#define MSG_BUFFER_SIZE (50)
+#define MQTT_MAX_PACKET_SIZE 512
 extern EQSP32 eqsp32;
 // Update these with values suitable for your network.
 const char *ssid = APPSSID;
@@ -14,56 +14,35 @@ WiFiClient espCustomClient;
 PubSubClient mqttClient(espCustomClient);
 
 unsigned long lastMsg = 0;
-char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
 extern char boardID[23];
-
-// void setup_wifi()
-// {
-//   delay(10);
-//   DebugSerial::print("Connecting to ");
-//   DebugSerial::println(ssid);
-
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin(ssid, password);
-
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     delay(500);
-//     DebugSerial::print(".");
-//   }
-
-//   randomSeed(micros());
-//   printWifiInfo();
-// }
 
 void reconnect()
 {
   startWatchDog();
   // Ensure WiFi is connected
-  while (!eqsp32.getWiFiStatus() == EQ_WF_CONNECTED)
+  while (!eqsp32.getWiFiStatus() == EQ_WF_CONNECTED && WiFi.localIP().toString() == "0.0.0.0")
   {
     DebugSerial::print(".");
     delay(500);
   }
   printWifiInfo();
-  checkDeviceExist();
 
   // Prepare will message
-  String willMessageStr = "{\"status\":\"disconnected\", \"client\":\"" + String(boardID) + "\", \"appver\":\"" + String(APPVERSION) + "\"}";
+  String willMessageStr = "{\"status\":\"disconnected\", \"client\":\"" + String(boardID) + "\", \"appver\":\"" + String(APPVERSION) + "\", \"appScreenSize\":\"" + String(APPSCREENSIZE) + "\", \"appUpdName\":\"" + String(APPUPDNAME) + "\", \"appDevType\":\"" + String(APPDEVTYPE) + "\"}";
   const char *willMessage = willMessageStr.c_str();
-
+  String fullTopic = String(APPPMQTTSTSTOPIC) + "/" + boardID;
   // Loop until we're reconnected to the MQTT broker
   while (!mqttClient.connected())
   {
     DebugSerial::print("Attempting MQTT connection...");
-
+    
     // Attempt to connect with all parameters
     if (mqttClient.connect(boardID,                          // Client ID
                            mqtt_user,                        // Username
                            mqtt_pass,                        // Password
-                           String(APPPMQTTSTSTOPIC).c_str(), // Will Topic
+                           fullTopic.c_str(), // Will Topic
                            2,                                // Will QoS
                            true,                             // Will Retain
                            willMessage,                      // Will Message
@@ -72,11 +51,13 @@ void reconnect()
       DebugSerial::println("MQTT Connected!");
 
       // Send connection acknowledgment
-      char dataToSend[72];
-      snprintf(dataToSend, sizeof(dataToSend),
-               "{\"status\":\"%s\", \"client\": \"%s\", \"appver\": \"%s\"}",
-               "connected", boardID, String(APPVERSION));
-      mqttClient.publish(String(APPPMQTTSTSTOPIC).c_str(), dataToSend, true);
+      char dataToSend[256];
+      String ipAddress = WiFi.localIP().toString();
+      snprintf(dataToSend, sizeof(dataToSend), 
+           "{\"status\":\"connected\",\"client\":\"%s\",\"ip\":\"%s\",\"appVersion\":\"%s\",\"appScreenSize\":\"%s\",\"appUpdName\":\"%s\",\"appDevType\":\"%s\"}", 
+           boardID, ipAddress.c_str(), APPVERSION, APPSCREENSIZE, APPUPDNAME, APPDEVTYPE);
+      
+      mqttClient.publish(fullTopic.c_str(), dataToSend, true);
 
       // Subscribe to command topics
       mqttClient.subscribe((String(APPPMQTTCMDTOPIC) + "/#").c_str());
@@ -115,6 +96,10 @@ void callback(char *topic, byte *payload, unsigned int length)
     {
       OTACheck(true);
     }
+    if (payloadStr == "SYNCNTP")
+    {
+      syncNTP();
+    }
     if (payloadStr == "STATUS")
     {
       char dataToSend[256];
@@ -123,7 +108,7 @@ void callback(char *topic, byte *payload, unsigned int length)
       String uptime = getUptime();
       String bootTime = getDateTimeFromUptime(millis() / 1000);
       snprintf(dataToSend, sizeof(dataToSend), 
-           "{\"mac\":\"%s\",\"ip\":\"%s\",\"uptime\":\"%s\",\"bootTime\":\"%s\",\"appVersion\":\"%s\",\"appScreenSize\":\"%s\",\"appUpdName\":\"%s\",\"appDevType\":\"%s\"}", 
+           "{\"client\":\"%s\",\"ip\":\"%s\",\"uptime\":\"%s\",\"bootTime\":\"%s\",\"appVersion\":\"%s\",\"appScreenSize\":\"%s\",\"appUpdName\":\"%s\",\"appDevType\":\"%s\"}", 
            boardID, ipAddress.c_str(), uptime.c_str(), bootTime.c_str(), APPVERSION, APPSCREENSIZE, APPUPDNAME, APPDEVTYPE);
 
       // Publish the data
@@ -157,7 +142,7 @@ void sendDataMQTT(const ChamberData& data)
 {
   char dataToSend[256];
   snprintf(dataToSend, sizeof(dataToSend),
-           "{\"sensor_id\":\"%s\",\"tempPV\":%.2f,\"tempSP\":%.2f,\"wetPV\":%.2f,\"wetSP\":%.2f,\"humiPV\":%.2f,\"humiSP\":%.2f,\"nowSTS\":%d}",
+           "{\"client\":\"%s\",\"tempPV\":%.2f,\"tempSP\":%.2f,\"wetPV\":%.2f,\"wetSP\":%.2f,\"humiPV\":%.2f,\"humiSP\":%.2f,\"nowSTS\":%d}",
            boardID, data.tempPV, data.tempSP, data.wetPV, data.wetSP, data.humiPV, data.humiSP, data.nowSTS);
 
   mqttClient.publish(String(APPPMQTTDATATOPIC).c_str(), dataToSend);
